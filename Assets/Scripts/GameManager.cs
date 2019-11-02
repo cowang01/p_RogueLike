0df37b6a73405204b6a9f0719bb8a0d5692c3899
cowang01 <g_cowan27@hotmail.com>
2019-11-02 13:36:06 +0000

    half-way

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public BoardManager boardScript;
+	public static GameManager instance = null;
+
+	private int level = 3;
+
+	void Awake(){
+		if (instance == null){
+			instance = this;
+		} else if (instance != this){
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+		boardScript = GetComponent<BoardManager>();
+		InitGame();
+	}
+
+	void InitGame(){
+		boardScript.SetUpScene(level);
+	}
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
